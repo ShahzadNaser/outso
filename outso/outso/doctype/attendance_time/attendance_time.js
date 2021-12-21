28e0880bb5d0b588502e1bf1afe65210ad047d3c
Shahzad Naser <shahzadnaser1122@gmail.com>
2021-12-21 21:57:40 +0500

    Add new doctype for recording of raw check time

---
+++ outso/outso/doctype/attendance_time/attendance_time.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Shahzad Naser and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Attendance Time', {
+	refresh: function(frm) {
+		frm.disable_save();
+	}
+});
