d862ca63022434435f6c6cb5b7bd2d4816693a60
Shahzad Naser <shahzadnaser1122@gmail.com>
2023-09-12 06:58:38 +0500

    update salary slip code for updating the calculation formula for income tax calculation

--- outso/modules/hr/salary_slip/salary_slip.py
+++ outso/modules/hr/salary_slip/salary_slip.py
@@ -3,7 +3,9 @@
 
 import frappe
 from frappe import _
-from frappe.utils import getdate, formatdate
+import math
+from frappe.utils import getdate, formatdate, flt
+from erpnext.payroll.doctype.payroll_period.payroll_period import get_period_factor
 from erpnext.payroll.doctype.salary_slip.salary_slip import SalarySlip
 import re
 
@@ -76,6 +78,64 @@ class CustomSalarySlip(SalarySlip):
         # customization add data in cache for performance imporvement
         #frappe.cache().set_value(key, data, expires_in_sec=100)
         return data
+
+    def calculate_variable_tax(self, payroll_period, tax_component):
+        # get Tax slab from salary structure assignment for the employee and payroll period
+        tax_slab = self.get_income_tax_slabs(payroll_period)
+
+        # get remaining numbers of sub-period (period for which one salary is processed)
+        remaining_sub_periods = get_period_factor(self.employee,
+            self.start_date, self.end_date, self.payroll_frequency, payroll_period)[1]
+        # get taxable_earnings, paid_taxes for previous period
+        previous_taxable_earnings = self.get_taxable_earnings_for_prev_period(payroll_period.start_date,
+            self.start_date, tax_slab.allow_tax_exemption)
+        previous_total_paid_taxes = self.get_tax_paid_in_period(payroll_period.start_date, self.start_date, tax_component)
+
+        # get taxable_earnings for current period (all days)
+        current_taxable_earnings = self.get_taxable_earnings(tax_slab.allow_tax_exemption)
+        future_structured_taxable_earnings = current_taxable_earnings.taxable_earnings * (math.ceil(remaining_sub_periods) - 1)
+
+        # get taxable_earnings, addition_earnings for current actual payment days
+        current_taxable_earnings_for_payment_days = self.get_taxable_earnings(tax_slab.allow_tax_exemption, based_on_payment_days=0)
+        current_structured_taxable_earnings = current_taxable_earnings_for_payment_days.taxable_earnings
+        current_additional_earnings = current_taxable_earnings_for_payment_days.additional_income
+        current_additional_earnings_with_full_tax = current_taxable_earnings_for_payment_days.additional_income_with_full_tax
+
+        # Get taxable unclaimed benefits
+        unclaimed_taxable_benefits = 0
+        if self.deduct_tax_for_unclaimed_employee_benefits:
+            unclaimed_taxable_benefits = self.calculate_unclaimed_taxable_benefits(payroll_period)
+            unclaimed_taxable_benefits += current_taxable_earnings_for_payment_days.flexi_benefits
+
+        # Total exemption amount based on tax exemption declaration
+        total_exemption_amount = self.get_total_exemption_amount(payroll_period, tax_slab)
+
+        #Employee Other Incomes
+        other_incomes = self.get_income_form_other_sources(payroll_period) or 0.0
+
+        # Total taxable earnings including additional and other incomes
+        total_taxable_earnings = previous_taxable_earnings + current_structured_taxable_earnings + future_structured_taxable_earnings \
+            + current_additional_earnings + other_incomes + unclaimed_taxable_benefits - total_exemption_amount
+
+        # Total taxable earnings without additional earnings with full tax
+        total_taxable_earnings_without_full_tax_addl_components = total_taxable_earnings - current_additional_earnings_with_full_tax
+
+        # Structured tax amount
+        total_structured_tax_amount = self.calculate_tax_by_tax_slab(
+            total_taxable_earnings_without_full_tax_addl_components, tax_slab)
+        current_structured_tax_amount = (total_structured_tax_amount - previous_total_paid_taxes) / remaining_sub_periods
+
+        # Total taxable earnings with additional earnings with full tax
+        full_tax_on_additional_earnings = 0.0
+        if current_additional_earnings_with_full_tax:
+            total_tax_amount = self.calculate_tax_by_tax_slab(total_taxable_earnings, tax_slab)
+            full_tax_on_additional_earnings = total_tax_amount - total_structured_tax_amount
+
+        current_tax_amount = current_structured_tax_amount + full_tax_on_additional_earnings
+        if flt(current_tax_amount) < 0:
+            current_tax_amount = 0
+        return current_tax_amount
+
 
 def calculate_pieces(self):
     completed_pieces_data = frappe.db.sql('''
